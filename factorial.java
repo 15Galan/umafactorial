3d672e81634583196d118272ef84231b4fe5f86e
15Galan <antoniogalan_15outlook.com>
2017-03-17 18:08:55 +0100

    Refactorización del método de entrada
    Introducción a la Ingeniería del Software: Práctica 1 (paso 14).

--- factorial.java
+++ factorial.java
@@ -10,22 +10,22 @@ return num*fact(num-1);
 }
 }
 
-public static void main(String []args) {
-		Scanner scanInput = new Scanner(System.in);
-		System.out.print("Introduzca un numero: ");
-		long num = scanInput.nextLong();
+ublic static void main(String []args) {
+			Scannerw Scanner(System.in);
+					System.out.print("Introduzca un numero: ");
+							long num = scanInput.nextLong();
 
-		if (num<0) {
-			System.out.println("El factorial no esta definido"
-+ " para numeros negativos");
-		} else {
-			long fac = fact(num);
+									if (num<0) {
+													System.out.println("El factorial no esta definido"
+															+ " para numeros negativos");
+															} else {
+																			long fac = fact(num);
 
-			System.out.println("El factorial de "+num+" es "+fac);
-		}
+																						System.out.println("El factorial de "+num+" es "+fac);
+																								}
 
-		scanInput.close();
-	}
+											scanInput.close();
+												}
 
 
 }

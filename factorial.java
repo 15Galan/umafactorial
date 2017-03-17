4e2154020938f482ac3184a31fecb6c747b420e9
15Galan <antoniogalan_15outlook.com>
2017-03-17 13:28:36 +0100

    Refactorización.
    Introducción a la Ingeniería del Software: Práctica 1 (paso 4).

--- factorial.java
+++ factorial.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 
 public class factorial {
-.git/
-.git/public static void main(String []args) {
-.git/Scanner scanInput = new Scanner(System.in);
-.git/System.out.print("Introduzca un numero: ");
-.git/long num = scanInput.nextLong();
-.git/long fac = 1;
-.git/for (long i=1; i<=num; ++i) {
-.git/fac = fac * i;
-.git/}
-.git/System.out.println("El factorial de "+num+" es "+fac);
-.git/scanInput.close();
-.git/}
+
+public static long fact(long num) {
+long fac = 1;
+for (long i=1; i<=num; ++i) {
+fac = fac * i;
+}
+return fac;
+}
+
+public static void main(String []args) {
+Scanner scanInput = new Scanner(System.in);
+System.out.print("Introduzca un numero: ");
+long num = scanInput.nextLong();
+
+long fac = fact(num);
+
+System.out.println("El factorial de "+num+" es "+fac);
+scanInput.close();
+}
 
 }
